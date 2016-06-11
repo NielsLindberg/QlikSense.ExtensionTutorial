2a674ec9969b7d6041629d7dca44dde976825b6c
Niels Lindberg <eighthourcream@gmail.com>
2016-06-11 19:26:23 +0200

    added npm and grunt build setup

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        copy: {
+            main: {
+                files: [{
+                    expand: false,
+                    cwd: '',
+                    src: ['HelloWorld.js','HelloWorld.qext'],
+                    dest: 'C:/Users/Niels/Documents/Qlik/Sense/Extensions/ExtensionTutorial/'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.registerTask('default', ['copy']);
+};
